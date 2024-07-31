7e66c9e202f27da11a989fb89487e7f3c5d6a94c
Emanuel Turis <hello@emanuelturis.com>
2024-07-31 16:21:08 +0300

    add tailwindcss colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./template/**/*.templ"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#1e1e1e",
+        primary: {
+          DEFAULT: "#2553c9",
+          dark: "#0938b3",
+        },
+        secondary: {
+          DEFAULT: "#f4a300",
+          dark: "#d68f01",
+        },
+      },
+    },
   },
   plugins: [],
 };
